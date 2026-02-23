95b39e27085b07252dd3d0e14f412a014d5ecffc
avvorkuntekin8-debug <avvorkuntekin8@gmail.com>
2026-02-23 16:37:22 +0300

    fix sqlite path

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_PATH
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DB_PATH
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
