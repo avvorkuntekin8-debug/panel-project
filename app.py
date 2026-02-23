57551bd364a171ee6e6c49f48a8a62e57693f43b
avvorkuntekin8-debug <avvorkuntekin8@gmail.com>
2026-02-23 16:27:26 +0300

    safe db config

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ from collections import defaultdict
 from flask import request, redirect, url_for
 import os
 
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, "prod_database.db")
+DB_PATH = os.path.join(BASE_DIR, "database.db")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_PATH
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 def send_admin_message(user_id, username, plan):
 
