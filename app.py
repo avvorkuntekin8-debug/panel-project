114ea6381ea952dde53d969f1ab7911a02012664
avvorkuntekin8-debug <avvorkuntekin8@gmail.com>
2026-02-23 16:18:26 +0300

    fix production db create

--- app.py
+++ app.py
@@ -48,11 +48,14 @@ Plan: {plan}
     })
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "fallback-secret")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_PATH
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "fallback")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///prod_database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -955,13 +958,9 @@ def user_dashboard():
         percent=percent
     )
     
- with app.app_context():
-    db.create_all()
  
 # =========================================================
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+if __name__ == "__main__":   
     app.run()
     
     
